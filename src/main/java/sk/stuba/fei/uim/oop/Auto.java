cc64a62115a2fd6e06b4ad6d52a6874c00e397d6
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-03-02 15:54:55 +0100

    metody, gettre/settre

--- src/main/java/sk/stuba/fei/uim/oop/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/Auto.java
@@ -5,12 +5,25 @@ public class Auto {
     public static final double PALIVO_NA_KM = 0.2;
     private double kapacitaNadrze;
     private double stavNadrze;
+    private boolean ojazdene;
 
     public void jazdi(double vzdialenost){
+        this.ojazdene = true;
         stavNadrze -= vzdialenost * PALIVO_NA_KM ;
         if(stavNadrze < 0){
             stavNadrze = 0;
         }
+    }
+
+    public String stav(){
+        String vysledok;
+        if (this.ojazdene){
+            vysledok = "Auto je ojazdene ";
+        }else{
+            vysledok = "Auto je neojazdene ";
+        }
+        vysledok += "stav nadrze: " + stavNadrze + "/" + kapacitaNadrze;
+        return vysledok;
     }
 
     public double getKapacitaNadrze(){
