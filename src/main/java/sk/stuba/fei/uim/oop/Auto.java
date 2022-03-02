e879a09a22f3e3e3e6e96f4f6d486f5439bb16b4
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-03-02 15:29:27 +0100

    Classy

---
+++ src/main/java/sk/stuba/fei/uim/oop/Auto.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop;
+
+
+public class Auto {
+    int pocetDveri;
+    int pocetKolies;
+    String nazov;
+
+    public int jedna;
+    int dva;
+    protected int tri;
+    private int styri;
+    }
---
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -0,0 +1,16 @@
+package sk.stuba.fei.uim.oop;
+
+import sk.stuba.fei.uim.oop.Auto;
+
+public class Main {
+    public static void main(String[] args) {
+        Auto skoda = new Auto();
+        Auto bmw = new Auto();
+
+        skoda.jedna = 1;
+        bmw.jedna = 1;
+
+        skoda.dva =2;
+        skoda.tri =3;
+    }
+}
