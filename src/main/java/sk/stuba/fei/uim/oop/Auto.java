1a032a2a056cc6addda2e763d6e9c14081cb32a1
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-03-02 16:00:47 +0100

    Konstruktory s viacerymi parametrami

--- src/main/java/sk/stuba/fei/uim/oop/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/Auto.java
@@ -1,11 +1,24 @@
 package sk.stuba.fei.uim.oop;
 
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Auto {
     public static final double PALIVO_NA_KM = 0.2;
+    public static final double KAPACITA_NADRZE = 100;
     private double kapacitaNadrze;
     private double stavNadrze;
     private boolean ojazdene;
+
+    public Auto(){
+        this(KAPACITA_NADRZE);
+    }
+
+    public Auto(double kapacitaNadrze){
+        this.ojazdene = true;
+        this.stavNadrze = 50;
+        this.kapacitaNadrze = kapacitaNadrze;
+    }
 
     public void jazdi(double vzdialenost){
         this.ojazdene = true;
--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -5,11 +5,18 @@ import sk.stuba.fei.uim.oop.Auto;
 public class Main {
     public static void main(String[] args) {
         Auto skoda = new Auto();
+        Auto vw = new Auto(50);
 
-        skoda.setStavNadrze(100);
-        skoda.setKapacitaNadrze(100);
+        System.out.println("skoda");
         System.out.println(skoda.stav());
         skoda.jazdi(100);
         System.out.println(skoda.stav());
+
+        System.out.println("vw");
+        System.out.println(skoda.stav());
+        skoda.jazdi(100);
+        System.out.println(skoda.stav());
+
     }
+
 }
