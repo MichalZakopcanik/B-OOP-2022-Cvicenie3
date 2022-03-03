5b4cf625ce63a6f0ac9aac02068a2d03c7f27b48
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-03-03 11:16:33 +0100

    Zklavesnice, KeyboardInput v1

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,21 +1,29 @@
 package sk.stuba.fei.uim.oop;
 
 import sk.stuba.fei.uim.oop.auto.Auto;
+import sk.stuba.fei.uim.oop.auto.Vozidlo;
+import sk.stuba.fei.uim.oop.utility.ZKlavesnice;
+import sk.stuba.fei.uim.oop.utility.KeyboardInput;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Auto skoda = new Auto();
-        Auto vw = new Auto(50);
+        int vek = ZKlavesnice.readInt("Zadajte vek:");
+        System.out.println("vek zklavesnice:" + vek);
 
-        System.out.println("skoda");
-        System.out.println(skoda.stav());
-        skoda.jazdi(100);
-        System.out.println(skoda.stav());
+        System.out.println("----------------------------");
 
-        System.out.println("vw");
-        System.out.println(skoda.stav());
-        skoda.jazdi(100);
-        System.out.println(skoda.stav());
+        vek = KeyboardInput.readInt("Zadajte vek:",2);
+        System.out.println("vek keyboardinput:" + vek);
+
+        System.out.println("----------------------------");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zadajte vek:");
+        vek = scanner.nextInt();
+
+        System.out.println("vek scanner:" + vek);
 
     }
 
