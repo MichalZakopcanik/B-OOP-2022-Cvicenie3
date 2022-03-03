d3efe52880be375702c2cd4aeffde70cc528b9fd
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-03-03 11:24:04 +0100

    scanner random, z klavesnice, keyboard input

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -9,21 +9,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int vek = ZKlavesnice.readInt("Zadajte vek:");
-        System.out.println("vek zklavesnice:" + vek);
-
-        System.out.println("----------------------------");
-
-        vek = KeyboardInput.readInt("Zadajte vek:",2);
-        System.out.println("vek keyboardinput:" + vek);
-
-        System.out.println("----------------------------");
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Zadajte vek:");
-        vek = scanner.nextInt();
-
-        System.out.println("vek scanner:" + vek);
+    Auto auto = new Auto();
+    auto.nacitaj();
 
     }
 
